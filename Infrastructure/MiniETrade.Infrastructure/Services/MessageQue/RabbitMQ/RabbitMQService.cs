ba935df888acb0145e9ead1349792ec3cdd61909
Hüseyin Akan <hakan@mngkargo.com.tr>
2023-04-11 16:39:54 +0300

    Rabbit MQ Message Designs

--- Infrastructure/MiniETrade.Infrastructure/Services/MessageQue/RabbitMQ/RabbitMQService.cs
+++ Infrastructure/MiniETrade.Infrastructure/Services/MessageQue/RabbitMQ/RabbitMQService.cs
@@ -96,6 +96,111 @@ namespace MiniETrade.Infrastructure.Services.MessageQue.RabbitMQ
             //};
             //channel.BasicPublish(exchange: "header-exchange-example", routingKey: string.Empty, body: message, basicProperties: basicProperties);
             #endregion
+
+            #region P2P (Point-to-Point) Tasarımı
+            //string queueName = "example-p2p-queue";
+
+            //channel.QueueDeclare(
+            //    queue: queueName,
+            //    durable: false,
+            //    exclusive: false,
+            //    autoDelete: false);
+
+            //byte[] message = Encoding.UTF8.GetBytes("merhaba");
+            //channel.BasicPublish(
+            //    exchange: string.Empty,
+            //    routingKey: queueName,
+            //    body: message);
+            #endregion
+
+            #region Publish/Subscribe (Pub/Sub) Tasarımı
+            //string exchangeName = "example-pub-sub-exchange";
+
+            //channel.ExchangeDeclare(
+            //    exchange: exchangeName,
+            //    type: ExchangeType.Fanout);
+
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    await Task.Delay(200);
+
+            //    byte[] message = Encoding.UTF8.GetBytes("merhaba" + i);
+
+            //    channel.BasicPublish(
+            //        exchange: exchangeName,
+            //        routingKey: string.Empty,
+            //        body: message);
+            //}
+
+            #endregion
+
+            #region Work Queue(İş Kuyruğu) Tasarımı​
+            //string queueName = "example-work-queue";
+
+            //channel.QueueDeclare(
+            //    queue: queueName,
+            //    durable: false,
+            //    exclusive: false,
+            //    autoDelete: false);
+
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    await Task.Delay(200);
+
+            //    byte[] message = Encoding.UTF8.GetBytes("merhaba" + i);
+
+            //    channel.BasicPublish(
+            //        exchange: string.Empty,
+            //        routingKey: queueName,
+            //        body: message);
+            //}
+
+            #endregion
+
+            #region Request/Response Tasarımı​
+            //string requestQueueName = "example-request-response-queue";
+            //channel.QueueDeclare(
+            //    queue: requestQueueName,
+            //    durable: false,
+            //    exclusive: false,
+            //    autoDelete: false);
+
+            //string replyQueueName = channel.QueueDeclare().QueueName;
+
+            //string correlationId = Guid.NewGuid().ToString();
+
+            //#region Request Mesajını Oluşturma ve Gönderme
+            //IBasicProperties properties = channel.CreateBasicProperties();
+            //properties.CorrelationId = correlationId;
+            //properties.ReplyTo = replyQueueName;
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    byte[] message = Encoding.UTF8.GetBytes("merhaba" + i);
+            //    channel.BasicPublish(
+            //        exchange: string.Empty,
+            //        routingKey: requestQueueName,
+            //        body: message,
+            //        basicProperties: properties);
+            //}
+            //#endregion
+            //#region Response Kuyruğu Dinleme
+            //EventingBasicConsumer consumer = new(channel);
+            //channel.BasicConsume(
+            //    queue: replyQueueName,
+            //    autoAck: true,
+            //    consumer: consumer);
+
+            //consumer.Received += (sender, e) =>
+            //{
+            //    if (e.BasicProperties.CorrelationId == correlationId)
+            //    {
+            //        //....
+            //        Console.WriteLine($"Response : {Encoding.UTF8.GetString(e.Body.Span)}");
+            //    }
+            //};
+            #endregion
+
         }
 
         public void Consume()
@@ -160,6 +265,114 @@ namespace MiniETrade.Infrastructure.Services.MessageQue.RabbitMQ
             //EventingBasicConsumer consumer5 = new(channel);
             //channel.BasicConsume(queue: queueName, autoAck: true, consumer5);
             #endregion
+
+            #region P2P (Point-to-Point) Tasarımı
+            //string queueName = "example-p2p-queue";
+
+            //channel.QueueDeclare(
+            //    queue: queueName,
+            //    durable: false,
+            //    exclusive: false,
+            //    autoDelete: false);
+
+            //EventingBasicConsumer consumer = new(channel);
+            //channel.BasicConsume(
+            //    queue: queueName,
+            //    autoAck: false,
+            //    consumer: consumer);
+
+            //consumer.Received += (sender, e) =>
+            //{
+            //    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+            //};
+            #endregion
+
+            #region Publish/Subscribe (Pub/Sub) Tasarımı
+            //string exchangeName = "example-pub-sub-exchange";
+
+            //channel.ExchangeDeclare(
+            //    exchange: exchangeName,
+            //    type: ExchangeType.Fanout);
+
+            //string queueName = channel.QueueDeclare().QueueName;
+            //channel.QueueBind(
+            //    queue: queueName,
+            //    exchange: exchangeName,
+            //    routingKey: string.Empty);
+
+            //EventingBasicConsumer consumer = new(channel);
+            //channel.BasicConsume(
+            //    queue: queueName,
+            //    autoAck: false,
+            //    consumer: consumer);
+
+            //consumer.Received += (sender, e) =>
+            //{
+            //    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+            //};
+
+
+            #endregion
+
+            #region Work Queue(İş Kuyruğu) Tasarımı​
+            //string queueName = "example-work-queue";
+
+            //channel.QueueDeclare(
+            //    queue: queueName,
+            //    durable: false,
+            //    exclusive: false,
+            //    autoDelete: false);
+
+            //EventingBasicConsumer consumer = new(channel);
+            //channel.BasicConsume(
+            //    queue: queueName,
+            //    autoAck: true,
+            //    consumer: consumer);
+
+            //channel.BasicQos(
+            //    prefetchCount: 1,
+            //    prefetchSize: 0,
+            //    global: false);
+
+            //consumer.Received += (sender, e) =>
+            //{
+            //    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+            //};
+            #endregion
+
+            #region Request/Response Tasarımı​
+
+            //string requestQueueName = "example-request-response-queue";
+            //channel.QueueDeclare(
+            //    queue: requestQueueName,
+            //    durable: false,
+            //    exclusive: false,
+            //    autoDelete: false);
+
+            //EventingBasicConsumer consumer7 = new(channel);
+            //channel.BasicConsume(
+            //    queue: requestQueueName,
+            //    autoAck: true,
+            //    consumer: consumer7);
+
+            //consumer7.Received += (sender, e) =>
+            //{
+            //    string message = Encoding.UTF8.GetString(e.Body.Span);
+            //    Console.WriteLine(message);
+            //    //.....
+            //    byte[] responseMessage = Encoding.UTF8.GetBytes($"İşlem tamamlandı. : {message}");
+            //    IBasicProperties properties = channel.CreateBasicProperties();
+            //    properties.CorrelationId = e.BasicProperties.CorrelationId;
+            //    channel.BasicPublish(
+            //        exchange: string.Empty,
+            //        routingKey: e.BasicProperties.ReplyTo,
+            //        basicProperties: properties,
+            //        body: responseMessage);
+            //};
+
+            #endregion
+
+
 
             consumer.Received += (sender, e) =>
             {
